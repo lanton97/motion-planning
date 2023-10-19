e519330852eace42eff07fa649d32acf9f3f320e
Luka Antonyshyn <15la@caslab.queensu.ca>
2023-10-19 15:13:31 -0400

    Add basic configuration collision checker

---
+++ collisionCheckers/base.py
@@ -0,0 +1,35 @@
+# Collision checker for sampling new configurations
+# We treat each configuration as a sphere/circle
+# of a given radius to simplify
+# New collision checkers can easily be implemented by extending this class
+# and overloading the functions
+class PointCollisionChecker():
+    def __init__(self,
+            positionDimensionality,
+            configurationDimensionality,
+            collDist,
+            ):
+        self.posDim = positionDimensionality
+        self.confDim = configurationDimensionality
+        self.collDist = collDist
+
+    def checkPointCollisions(self, config, pointList):
+        pos = np.array(config[:self.posDim])
+        for point in pointList:
+            dist = np.linalg.norm(pos - np.array(point))
+            if dist <= self.collDist:
+                return True
+        return False
+
+    def checkWallCollision(self, config, wallList):
+        pos = np.array(config[:self.posDim])
+        for wall in wallList:
+            dist = np.linalg.norm(pos - np.array(point))
+            if dist <= self.collDist:
+                return True
+        return False
+
+
+
+
+
