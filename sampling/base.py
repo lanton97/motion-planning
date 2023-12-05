6a0e4ff0c8f8f5f0a96584cf48f3a20341476bf8
Luka Antonyshyn <15la@caslab.queensu.ca>
2023-12-05 16:43:40 -0500

    Update virtual files and add base planner comments

--- sampling/base.py
+++ sampling/base.py
@@ -1,39 +1,46 @@
 import numpy as np
+from viz.viz import BaseRenderer
 
+# A simple base planner acting as a virtual class
 class BaseSamplingPlanner():
-    def __init__(self,
-            environment,
-            dynamics,
-            ):
-        self.env = environment
-        self.dyn = dynamics
-
+    def __init__(self):
+        pass
+        
     def plan(self):
         raise NotImplementedError
+
+    def render(self):
+        pass
 
 class ConfigurationNode():
     def __init__(self, configuration):
         self.config = configuration
 
-
+# This graph is used to hold the configurations and connections
+# for sampling based planners
 class ConfigurationGraph():
     nodes = list()
     edges = set()
-    def __init__(self, dimensionality, initNode):
-        self.dim = dimensionality
+    def __init__(self, configDimensionality, positionDimensionality, initNode):
+        self.configDim = configDimensionality
+        self.posDim = positionDimensionality
         self.nodes.append(initNode)
     
+    # Add a new configuration node to the graph, as well as a new edge
+    # Between the node and it's parent
     def addNode(self,
             parentNode: ConfigurationNode,
             node: ConfigurationNode):
         self.nodes.append(node)
         self.edges.add((parentNode, Node))
 
+    # Return the node with the configuration closest to the given position
     def getNearestNode(self, position):
         minDist = inf
         nearestNode = None
         for node in self.nodes:
-            dist = np.linalg.norm(position - node.config[:self.dim])
+            # The default np norm is l2, i.e. the distance
+            dist = np.linalg.norm(position - node.config[:self.posDim])
             if dist < minDist:
                 minDist = dist
                 nearestNode = node
