b99520dec6c1987f7b6ec04cef6aa5fc13cfea42
Luka Antonyshyn <15la@caslab.queensu.ca>
2023-12-11 21:18:30 -0500

    Add utility functions necessary to convey paths in analytical forms

--- vehicles/ext/dubins.py
+++ vehicles/ext/dubins.py
@@ -19,10 +19,24 @@ TODOS:
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import Arc
 import math
 import numpy as np
 from enum import Enum
 
+def getTurnCentres(start, target, turnRad):
+    lstart_th = start[2] + np.pi/2
+    rstart_th = start[2] - np.pi/2
+    lend_th   = target[2] + np.pi/2
+    rend_th   = target[2] - np.pi/2
+
+    # Calculate the possible turn centres for the start and end turns
+    lstart = np.array([start[0] + np.cos(lstart_th)*turnRad, start[1] + np.sin(lstart_th)*turnRad])
+    rstart = np.array([start[0] + np.cos(rstart_th)*turnRad, start[1] + np.sin(rstart_th)*turnRad])
+    lend = np.array([target[0] + np.cos(lend_th)*turnRad, target[1] + np.sin(lend_th)*turnRad])
+    rend = np.array([target[0] + np.cos(rend_th)*turnRad, target[1] + np.sin(rend_th)*turnRad])
+
+    return lstart, rstart, lend, rend
 
 class TurnType(Enum):
     LSL = 1
@@ -67,6 +81,12 @@ def wrapTo180(angle):
     if(q):
         angle = wrapTo360(angle + 180) - 180
     return angle
+
+def rad2Deg(radians):
+    return radians*180/np.pi
+
+def deg2Rad(degrees):
+    return degrees/180*np.pi
 
 def headingToStandard(hdg):
     # Convert NED heading to standard unit cirlce...degrees only for now (Im lazy)
@@ -114,6 +134,7 @@ def calcDubinsPath(wpt1, wpt2, turn_radius):
                 best_word = x+1
                 best_cost = cost
                 param.seg_final = [tz[x],pz[x],qz[x]]
+                #param.seg_final = [tz[x]*param.turn_radius,pz[x]*param.turn_radius,qz[x]*param.turn_radius]
 
     param.type = TurnType(best_word)
     return param
@@ -124,7 +145,6 @@ def dubinsLSL(alpha, beta, d):
     tmp1      = math.atan2((math.cos(beta)-math.cos(alpha)),tmp0)
     p_squared = 2 + d*d - (2*math.cos(alpha-beta)) + (2*d*(math.sin(alpha)-math.sin(beta)))
     if p_squared<0:
-        print('No LSL Path')
         p=-1
         q=-1
         t=-1
@@ -139,7 +159,6 @@ def dubinsRSR(alpha, beta, d):
     tmp1      = math.atan2((math.cos(alpha)-math.cos(beta)),tmp0)
     p_squared = 2 + d*d - (2*math.cos(alpha-beta)) + 2*d*(math.sin(beta)-math.sin(alpha))
     if p_squared<0:
-        print('No RSR Path')
         p=-1
         q=-1
         t=-1
@@ -153,7 +172,6 @@ def dubinsRSL(alpha,beta,d):
     tmp0      = d - math.sin(alpha) - math.sin(beta)
     p_squared = -2 + d*d + 2*math.cos(alpha-beta) - 2*d*(math.sin(alpha) + math.sin(beta))
     if p_squared<0:
-        print('No RSL Path')
         p=-1
         q=-1
         t=-1
@@ -168,7 +186,6 @@ def dubinsLSR(alpha, beta, d):
     tmp0      = d + math.sin(alpha) + math.sin(beta)
     p_squared = -2 + d*d + 2*math.cos(alpha-beta) + 2*d*(math.sin(alpha) + math.sin(beta))
     if p_squared<0:
-        print('No LSR Path')
         p=-1
         q=-1
         t=-1
@@ -182,7 +199,6 @@ def dubinsLSR(alpha, beta, d):
 def dubinsRLR(alpha, beta, d):
     tmp_rlr = (6 - d*d + 2*math.cos(alpha-beta) + 2*d*(math.sin(alpha)-math.sin(beta)))/8
     if(abs(tmp_rlr)>1):
-        print('No RLR Path')
         p=-1
         q=-1
         t=-1
@@ -196,7 +212,6 @@ def dubinsRLR(alpha, beta, d):
 def dubinsLRL(alpha, beta, d):
     tmp_lrl = (6 - d*d + 2*math.cos(alpha-beta) + 2*d*(-1*math.sin(alpha)+math.sin(beta)))/8
     if(abs(tmp_lrl)>1):
-        print('No LRL Path')
         p=-1
         q=-1
         t=-1
@@ -204,7 +219,6 @@ def dubinsLRL(alpha, beta, d):
         p = (2*math.pi - math.acos(tmp_lrl)) % (2*math.pi)
         t = (-1*alpha - math.atan2((math.cos(alpha)-math.cos(beta)), d+math.sin(alpha)-math.sin(beta)) + p/2) % (2*math.pi)
         q = ((beta % (2*math.pi))-alpha-t+(p % (2*math.pi))) % (2*math.pi)
-        print(t,p,q,beta,alpha)
     return t, p, q
 
 def dubins_traj(param,step):
@@ -214,12 +228,25 @@ def dubins_traj(param,step):
     length = (param.seg_final[0]+param.seg_final[1]+param.seg_final[2])*param.turn_radius
     length = math.floor(length/step)
     path = -1 * np.ones((length,3))
+    seg1_end = None
+    seg2_end = None
+    seg3_end = None
+    param1 = param.seg_final[0]
+    param2 = param.seg_final[1]
 
     while x < length:
         path[i] = dubins_path(param,x)
+        tprime = x / param.turn_radius
+        if(tprime<param1):
+            seg1_end = path[i]
+        elif(tprime<(param1+param2)):
+            seg2_end = path[i]
+        else:
+            seg3_end = path[i]
         x += step
         i+=1
-    return path
+
+    return path, seg1_end, seg2_end, seg3_end
 
 
 def dubins_path(param, t):
@@ -272,6 +299,24 @@ def dubins_segment(seg_param, seg_init, seg_type):
 
     return seg_end
 
+def centers(p1, p2, r):
+    x1, y1, a1 = p1 
+    x2, y2, a2 = p2 
+
+    q = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    x3 = (x1 + x2) / 2
+    y3 = (y1 + y2) / 2
+
+    xx = (r ** 2 - (q / 2) ** 2) ** 0.5 * (y1 - y2) / q
+    yy = (r ** 2 - (q / 2) ** 2) ** 0.5 * (x2 - x1) / q
+    return ((x3 + xx, y3 + yy), (x3 - xx, y3 - yy))
+
+def getTheta(centre, point):
+    x, y = [point[0]-centre[0], point[1]-centre[1]]
+    theta = np.arctan2(y, x)
+    return theta
+
+
 def main():
     # User's waypoints: [x, y, heading (degrees)]
     pt1 = Waypoint(0,0,0)
@@ -281,27 +326,61 @@ def main():
     Wptz = [pt1, pt2]
     # Run the code
     i = 0
+    fig, ax = plt.subplots()
 
     while i<len(Wptz)-1:
         param = calcDubinsPath(Wptz[i], Wptz[i+1], 5)
-        path = dubins_traj(param,1)
-        print(param.seg_final)
-
+        path, seg1_end, seg2_end, seg3_end = dubins_traj(param,1)
 
 
         # Plot the results
-        plt.plot(Wptz[i].x,Wptz[i].y,'kx')
-        plt.plot(Wptz[i+1].x,Wptz[i+1].y,'kx')
-        plt.plot(path[:,0],path[:,1],'b-')
+        #ax.plot(Wptz[i].x,Wptz[i].y,'kx')
+        #ax.plot(Wptz[i+1].x,Wptz[i+1].y,'kx')
+        #ax.plot(path[:,0],path[:,1],'b-')
         i+=1
-    
-    print(param.turn_radius)
 
-    plt.grid(True)
-    plt.axis("equal")
-    plt.title('Dubin\'s Curves Trajectory Generation')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    # Calculate possible circles
+    '''q = sqrt((x2-x1)^2 + (y2-y1)^2)
+
+    y3 = (y1+y2)/2
+
+    x3 = (x1+x2)/2 
+
+    x = x3 + sqrt(r^2-(q/2)^2)*(y1-y2)/q
+
+    y = y3 + sqrt(r^2-(q/2)^2)*(x2-x1)/q  
+
+    x = x3 - sqrt(r^2-(q/2)^2)*(y1-y2)/q
+
+    y = y3 - sqrt(r^2-(q/2)^2)*(x2-x1)/q  '''
+    
+    begin_centres = centers([0,0, 0], seg1_end, param.turn_radius)
+    centres = centers(seg2_end, seg3_end, param.turn_radius)
+
+    th1 = getTheta(centres[0], seg2_end)
+    th2 = getTheta(centres[0], seg3_end)
+
+    #patch1 = Arc(centres[0], 10.0, 10.0, theta1=(seg2_end[2] + np.pi/2)*180/np.pi, theta2=(seg3_end[2] + np.pi/2)*180/np.pi, color='red', lw=1)
+    patch1 = Arc(centres[0], 10.0, 10.0, theta1=(th1)*180/np.pi, theta2=(th2)*180/np.pi, color='red', lw=1)
+
+    th2 = getTheta(begin_centres[1], path[0])
+    th1 = getTheta(begin_centres[1], seg1_end)
+    patch2 = Arc(begin_centres[1], 10.0, 10.0, theta1=(th1)*180/np.pi, theta2=(th2)*180/np.pi, color='red', lw=1)
+
+
+
+    #ax.plot([0, seg1_end[0]], [0, seg1_end[1]]) 
+    ax.plot([seg1_end[0], seg2_end[0]], [seg1_end[1], seg2_end[1]], 'r-') 
+    #ax.plot([seg2_end[0], seg3_end[0]], [seg2_end[1], seg3_end[1]], 'r-') 
+
+    ax.add_patch(patch1)
+    ax.add_patch(patch2)
+
+    #plt.grid(True)
+    #plt.axis("equal")
+    #plt.title('Dubin\'s Curves Trajectory Generation')
+    #plt.xlabel('X')
+    #plt.ylabel('Y')
     plt.show()
 
 
